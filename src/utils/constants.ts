f651e8d0bc832f3e9f1143739913dd024858ff0e
tenoxious <tenox.0274@gmail.com>
2022-12-05 00:06:08 -0800

    update constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,17 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export default {
   RESERVOIR_ICON:
     "https://cdn.discordapp.com/icons/872790973309153280/0dc1b70867aeeb2ee32563f575c191c6.webp?size=4096",
   ALERT_COOLDOWN: 60 * 30, // 30 minute cooldown
   PRICE_CHANGE_OVERRIDE: 0.1, // 10% price change
-  ALERT_ENABLED: { listings: false, sales: false, floor: true, bid: true }, // enable alerts
-  TRACKED_CONTRACTS: [],
+  ALERT_ENABLED: { listings: true, sales: true, floor: true, bid: true }, // enable alerts
+  TRACKED_CONTRACTS: ["0x7afe30cb3e53dba6801aa0ea647a0ecea7cbe18d"],
   CHANNEL_IDS: {
-    mainChannel: "1037386291781840977",
-    listingChannel: "11111111111111111111",
-    salesChannel: "11111111111111111111",
+    mainChannel: process.env.DISCORD_MAIN_CHANNEL as string,
+    listingChannel: process.env.DISCORD_LISTING_CHANNEL as string,
+    salesChannel: process.env.DISCORD_SALES_CHANNEL as string
   },
-  ALERT_CONTRACT: "0x521f9c7505005cfa19a8e5786a9c3c9c9f5e6f42",
-  APPLICATION_ID: "1033117008667099157",
-  REDIS_HOST: "127.0.0.1",
-  REDIS_PORT: 6379,
+  ALERT_CONTRACT: "0x7afe30cb3e53dba6801aa0ea647a0ecea7cbe18d",
+  APPLICATION_ID: process.env.DISCORD_APP_ID as string,
+  REDIS_HOST: process.env.REDIS_HOST ?? "127.0.0.1",
+  REDIS_PORT: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379
 };
